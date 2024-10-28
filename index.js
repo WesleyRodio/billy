00e1bed63fc741d6d548cd6a617707b629d9b61f
= <wesleynicolas1506@gmail.com>
2024-10-28 18:41:12 -0300

    Billy 1.0

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
     if (now < expirationTime) {
       const expiredTimestamp = Math.round(expirationTime / 1_000);
       console.log(expiredTimestamp);
-      
+
       return interaction.reply({
         content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`,
         ephemeral: true,
